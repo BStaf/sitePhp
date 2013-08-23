889ac70dccd97553ec92b07b0d31889f277193a1
BTac <BTac42@gmail.com>
2013-08-23 16:54:33 -0400

    rewrote writeBlog.php
    rewrote wrtieBlog as a class
    now BlogEditor calls itself to add a blog post

--- BlogEditor.php
+++ BlogEditor.php
@@ -1,28 +1,65 @@
 <html>
 <body>
 
-<form action="writeBlog.php" method="post"
+<form action="BlogEditor.php" method="post"
 enctype="multipart/form-data">
 
+<?php
+//include php script with database login info
+include 'writeBlog.php'; 
+$SUBMIT_NOT_PRESSED = 0;
+$EMPTY_USER = 1;
+$EMPTY_PASS = 2;
+$checkUser = true;
+$checkUserHandler = 0;
+$user = "";
+$pass = "";
+if ( isset($_POST["user"]) ) $user = $_POST["user"];
+if ( isset($_POST["passw"]) ) $pass = $_POST["passw"];
+If ((!checkInput($user, $MAXUSERLEN)) || (!checkInput($pass, $MAXPASSLEN))){
+	$checkUser = false;
+}
 
-User: <input type="text" name="user">
-Password: <input type="text" name="passw">
+
+
+echo "User: <input type='text' name='user' value='$user'> 
+Password: <input type='password' name='passw' value='$pass'>
 <hr>
 <br>
-Title: <input type="text" size="50" name="title">
+Title: <input type='text' size='50' name='title'>
 <br>
-<textarea rows="30" cols="80" name="bentry">
+<textarea rows='30' cols='80' name='bentry'>
 
 </textarea>
 <br>
+
 Pictures to Upload:
-<div id="PicUpload">
-<label for="file">Picture:</label>
-<input type="file" name="Pic1" id="Pic1">
+<div id='PicUpload'>
+<label for='file'>Picture:</label>
+<input type='file' name='Pic1' id='Pic1'>
 <br>
 </div>
-<input type="submit">
-</form>
-
+<input type='submit' name='dataSubmit' value='1'>
+</form>";
+if ($checkUser){
+	$blogWrt = new wrtieBlog();
+	if ($blogWrt->dbConnect($DatabaseAddress,$DatabasePort,$DatabaseName,$DatabaseUser,$DatabasePass)){
+		if ($blogWrt->checkBlogLogin($user,$pass)){
+			echo "Username is valid<br>";
+			$blogWrt->writeBlog($UserID, $_POST["title"],$_POST["bentry"]);
+		}
+	}
+}
+//call database connect function with parameters set in dbConfig.php
+/*$DBConnection = dbConnect($DatabaseAddress,$DatabasePort,$DatabaseName,$DatabaseUser,$DatabasePass);
+if (!isset($DBConnection)){
+	echo "Cannot connect to database<br>";
+	exit;
+}
+if ( ($checkUser) && (checkBlogLogin($DBConnection,$user,$pass)) ){
+	echo "Username is valid<br>";
+}
+else*/
+?>
 </body>
 </html>
--- writeBlog.php
+++ writeBlog.php
@@ -1,89 +1,74 @@
-<html>
-<body>
+
 <?php
 //include php script with database login info
 include 'dbConfig.php'; 
+
 //returns true if input is valid
 //input string to be tested and max length of that string
 //this function does allow alphanumeric, and some special characters, 
 //but does not allow "; ( ) "
+$MAXUSERLEN = 15;
+$MAXPASSLEN = 30;
 function checkInput($string, $MaxLen){
 	//ctype_alnum($user)
 	//check for empty string
-	if (empty($string)){return false;}
+	if (empty($string)){echo "empty str"; return false;}
 	//check if string is too long
-	if (strlen($string) > $MaxLen){return false;}
+	if (strlen($string) > $MaxLen){echo "max length"; return false;}
 	//check for valid characters	
-	else if(!preg_match('/^[A-Za-z0-9_~\-!@#\$%\^&\*]+$/',$string)){return false;}
-	else {return true;}
+	else if(!preg_match('/^[A-Za-z0-9_~\-!@#\$%\^&\*]+$/',$string)){echo "invalid Character"; return false;}
+	else {echo "input OKr";return true;}
 }
 
-//database connect function
-function dbConnect($DBAddr,$DBPort,$DBName,$DBUser,$DBPass){
-	// Connecting, selecting database  
-	//if ($DBPort = 0) {$DBPort = 3306;}
-	if (!$DBConnect = mysqli_connect($DBAddr,$DBUser,$DBPass,$DBName,$DBPort)) { 
-		throw new Exception('Failed to connect to Server' . mysql_error()); 
-		exit; 
-	} 
-	//echo 'Connected successfully'; 
-	//if (!mysqli_select_db($DBConect,$DBName)){  
-	//	echo('Could not select database');
-	//	return -1;
-	return $DBConnect;
-}
-
-function dbQuery($DBConnect, $Query){
-	// Query all pump station data 
-	//$query = 'SELECT * FROM LasaData'; 
-	if (!$result = mysqli_query($DBConnect,$Query)) {  
-		throw new Exception('Query failed: ' . mysql_error()); 
-		return -1;
-	} 
-	return $result;
-}
-  
-//parse data 
-//$row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
-  
-//pleasure road data 
-//$PR_TotFlow = number_format($row['PleasureRoad.PR_PLC.Global.FMain_Total_Flow']); 	
+class wrtieBlog {
+	private $DBConnect = null;
 	
+	public function __construct() {}
 	
-$MAXUSERLEN = 15;
-$MAXPASSLEN = 30;
-$user = $_POST["user"];
-$pass = $_POST["passw"];
-
-
-If ((!checkInput($user, $MAXUSERLEN)) || (!checkInput($pass, $MAXPASSLEN))){
-	exit("user name or password invalid");
-}
-$DTNow = date("Y-m-d H:i:s");
-echo "User $user writes on $DTNow:<br>";
-
-//call database connect function with parameters set in dbConfig.php
-$DBConnection = dbConnect($DatabaseAddress,$DatabasePort,$DatabaseName,$DatabaseUser,$DatabasePass);
-
-$UserQResult = dbQuery($DBConnection, 'SELECT * FROM Users');
-//$row = mysqli_fetch_array($UserQResult, MYSQLI_ASSOC); 
-$userValid = false;
-while($row = mysqli_fetch_array($UserQResult)){
-	if ($user == $row['userName']){
-		if ($pass == $row['password']){
-			$userValid = true;
+	//database connect function
+	public function dbConnect($DBAddr,$DBPort,$DBName,$DBUser,$DBPass){
+		// Connecting, selecting database  
+		if (!$this->DBConnect = mysqli_connect($DBAddr,$DBUser,$DBPass,$DBName,$DBPort)) { 
+			throw new Exception('Failed to connect to Server' . mysql_error()); 
+			return false;
+		} 
+		return true;
+	}
+	//querry function
+	private function dbQuery(/*$DBConnect, */$Query){
+		if (!$result = mysqli_query($this->DBConnect,$Query)) {  
+			throw new Exception('Query failed: ' . mysql_error()); 
+			return -1;
+		} 
+		return $result;
+	}
+	//checks user exists and has the same password.
+	//If user exists and is validated, returns user key ID, if not return -1
+	function checkBlogLogin(/*$DBConnect,*/ $bUser, $bPass){
+		$UserQResult = $this->dbQuery(/*$DBConnect,*/'SELECT * FROM Users'); 
+		//$userValid = false;
+		while($row = mysqli_fetch_array($UserQResult)){
+			if ($bUser == $row['userName']){
+				if ($bPass == $row['password']){
+					if ($row['idUsers'])//check that id is not null, it shouldn't be
+						return $row['idUsers'];
+					else
+						return -1;
+				}
+			}		
 		}
-	}		
+		return -1;
+	}
+	
+	function writeBlog($UserID,$title,$body){
+		$body = preg_replace("/\r/\n", "<br>", $body);
+		$Query = "INSERT INTO blogData (data) VALUES ('".$body."')";
+		$Result = $this->dbQuery($Query);
+		$lastKey = mysqli_insert_id($this->DBConnect);
+		if ($lastKey > 0){
+			$Query = "INSERT INTO blogEntry (blogTitle, timeStampCreated, idblogData) VALUES ('".$title."', '".date("Y-m-d H:i:s")."',".$lastKey.")";
+			$Result = $this->dbQuery($Query);
+		}
+	}
 }
-if ($userValid)
-	echo "Username is valid<br>";
-
 ?>
-<?php
-$string = preg_replace("/\r/\n", "<br>", $_POST["bentry"]);
-echo $string;
-
-?>
-
-</body>
-</html>
